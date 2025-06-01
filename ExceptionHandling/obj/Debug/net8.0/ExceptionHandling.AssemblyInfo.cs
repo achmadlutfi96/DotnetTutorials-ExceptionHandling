bf9533f373b1a6231e39f0453d1139350f7538c2
Achmad Lutfi <alhacker1996@gmail.com>
2025-06-01 16:07:38 +0700

    second commit

--- ExceptionHandling/obj/Debug/net8.0/ExceptionHandling.AssemblyInfo.cs
+++ ExceptionHandling/obj/Debug/net8.0/ExceptionHandling.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExceptionHandling")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69d9cf1232adad5370757751449cf8cf9cffb11")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExceptionHandling")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExceptionHandling")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
